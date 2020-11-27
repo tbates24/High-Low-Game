59af182399000f858e0b7d02f19869720ee241ac
tbates24 <bates.torrence@gmail.com>
2020-11-27 13:16:02 -0500

    new commit

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# High-Low-Game
-Game
+# High-Low Game
+
+
+
+## About
+The High-Low Game is a simple web application (using HTML5, CSS, and JS - no js frameworks were used) that involves a player versus the computer, or the "Dealer". The Dealer chooses a number between 1 and 100 (unbeknownst to the player). The user then chooses their own number between 1 and 100. After choosing their own number, the player then guesses if their number is either higher or lower than the Dealer's number. After the winner or loser is decided, the Dealer's number will be revealed. 
+
+
+## Installation
+
+To run the High-Low Game easily, you should have access to an IDE, I used VS Code and installed the Live Server extension within the IDE. However, it is possible to run the game locally without a server. If you choose to download VS Code, here is the link to get started:
+
+
+[VSCode - https://code.visualstudio.com/download ](https://code.visualstudio.com/download)
+
+
+## Objectives for the Web Application
+- Demonstrate use of localStorage
+- Field validation
+- Random Number Generation
+- DOM event usage
+
+## Game View
+
+![High-Low Game](img/High-Low.PNG)
+
+## Usage
+Here is a snippet of the function executed when the user clicks on the "High Button" and the player has selected a number
+```script
+function playerHighChoice() {//High Button
+  localStorage.getItem("player", pNum)
+  localStorage.getItem("dealer", dealer)
+  if (pNum > dealer) {
+    document.getElementById("Win").innerHTML = "<h1>YOU WON!!! LEEETTTSSS GOOOOOOO  :D</h1>";
+  } else if (pNum < dealer) {
+    document.getElementById("Lost").innerHTML = "<h2>YOU LOST!! BOO HOO MCGOO :(</h2>";
+  }
+}
+```
+![High-Low Game Console](img/HighLowConsole.PNG)
+## Special Thanks
+I would like to give a special thanks to Jae Johns. Found this cool animation that I thought would go well with the rest of my CSS. 
+
+
+[CSS Animation - https://medium.com/@jaejohns/9-incredibly-gorgeous-text-animations-in-css-f856cd20f81c](https://medium.com/@jaejohns/9-incredibly-gorgeous-text-animations-in-css-f856cd20f81c)
+
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## Experience
+I am an up and coming web developer. This is my first public project published on GitHub.
+I was interested in learning about localStorage, events, and the DOM. I thought this game would demonstrate some of those features. 
---
+++ js/High_Low.js
@@ -0,0 +1,140 @@
+//Buttons and Input Field
+
+document.getElementById("input-bar").addEventListener("click", startGame);
+document
+  .getElementById("player-choice")
+  .addEventListener("input", playerChoice);
+document.getElementById("high-btn").addEventListener("click", playerHighChoice);
+document.getElementById("low-btn").addEventListener("click", playerLowChoice);
+
+const backdropElement = document.getElementById("backdrop");
+const modalLinkElements = document.querySelectorAll(".info-modal");
+let infoModal;
+
+var dealer;
+var randNum; //Dealer's number
+var pNum; //Player's Number
+
+function randomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  randNum = Math.floor(Math.random() * (max - min) + min);
+  return randNum; //Returns the random number for the dealer
+}
+dealer = randomInt(2, 99); //Dealer will choose a number between 1 and 100
+
+function startGame() {
+  //Dealer's number is stored in local storage once the user clicks the "Start Game!" button
+  document.getElementById("input-bar").value = dealer;
+  localStorage.setItem("dealer", dealer);
+}
+
+function playerChoice() {
+  pNum = document.getElementById("player-choice").value;
+  if (pNum > 0 && pNum < 100) {
+  } else {
+    document.innerHTML = "<h2>Sorry, you need to choose a number between 1 and 100, otherwise i wont work<h2>"
+    document.getElementById("player-choice").value = false
+    // alert("Choose a number between 1 and 100");
+  }
+  localStorage.setItem("player", pNum); //Player's number is also stored in local storage once chosen
+  return pNum;
+}
+
+//The player and dealer numbers are taken from local storage to apply the logic below
+
+function playerHighChoice() {
+  //High Button
+  localStorage.getItem("player", pNum);
+  localStorage.getItem("dealer", dealer);
+  if (pNum > dealer) {
+    document.getElementById("Win").innerHTML =
+      "<h1>YOU WON!!! LEEETTTSSS GOOOOOOO  :D</h1>" + "<h1>The Dealer's Number was " + `${dealer}</h1`;
+  } else if (pNum < dealer) {
+    document.getElementById("Lost").innerHTML =
+      "<h2>YOU LOST!! BOO HOO MCGOO :(</h2>" + "<h2>The Dealer's Number was " + `${dealer}</h2>`;
+  }
+  document.getElementById("low-btn").disabled = true;
+}
+
+function playerLowChoice() {
+  //Low Button
+  localStorage.getItem("player", pNum);
+  localStorage.getItem("dealer", dealer);
+  if (pNum < dealer) {
+    document.getElementById("Win").innerHTML =
+      "<h1>YOU WON!!! LEEETTTSSS GOOOOOOO  :D</h1>" + "<h1>The Dealer's Number was " + `${dealer}</h1>`
+    
+  } else if (pNum > dealer) {
+    document.getElementById("Lost").innerHTML =
+      "<h2>YOU LOST!! BOO HOO MCGOO :(</h2>" + "<h2>The Dealer's Number was " + `${dealer}</h2>`;
+  }
+  
+  document.getElementById("high-btn").disabled = true;
+}
+
+//HTML animation function
+window.onload = function () {
+  animateSequence();
+};
+
+function animateSequence() {
+  var a = document.getElementsByClassName("sequence");
+  for (var i = 0; i < a.length; i++) {
+    var $this = a[i];
+    var letter = $this.innerHTML;
+    letter = letter.trim();
+    var str = "";
+    var delay = 100;
+    for (l = 0; l < letter.length; l++) {
+      if (letter[l] != " ") {
+        str +=
+          '<span style="animation-delay:' +
+          delay +
+          "ms; -moz-animation-delay:" +
+          delay +
+          "ms; -webkit-animation-delay:" +
+          delay +
+          'ms; ">' +
+          letter[l] +
+          "</span>";
+        delay += 50;
+      } else str += letter[l];
+    }
+    $this.innerHTML = str;
+  }
+}
+
+function toggleBackdrop() {
+  // backdropElement.classList.toggle('visible');
+}
+
+function presentInfoModal(event) {
+  // const text = event.target.dataset.text;
+  toggleBackdrop();
+  infoModal = document.createElement("div");
+  infoModal.classList.add("modal");
+  infoModal.innerHTML = `
+    <h2>Sorry, you need to choose a number between 1 and 100, otherwise i wont work</h2>
+   
+  `;
+  const closeButton = document.createElement("button");
+  closeButton.addEventListener("click", hideInfoModal);
+  closeButton.textContent = "Okay";
+  infoModal.appendChild(closeButton);
+  document.body.appendChild(infoModal);
+}
+
+function hideInfoModal() {
+  toggleBackdrop();
+  document.body.removeChild(infoModal);
+}
+
+for (const linkElement of modalLinkElements) {
+  linkElement.addEventListener("click", presentInfoModal);
+}
+
+// backdropElement.addEventListener('click', hideInfoModal);
+
+startGame(localStorage.clear());
+console.log(dealer); //For coder to see the random number in the console
